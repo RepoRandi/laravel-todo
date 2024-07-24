f00783a3f55dcc1a80c0d8aca56ec571e8fbdc19
Randi <randimaulanaakbar@gmail.com>
2024-07-24 17:02:49 +0700

    feat: add route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\Todo\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/todo', function () {
-    return view('todo.app');
-});
+Route::get('/todo', [TodoController::class, 'index']);
